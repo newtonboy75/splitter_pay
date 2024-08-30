a24ab4121580f8700bfcd941684f3734802004fb
Newton Tugadi <newtonboy@gmail.com>
2024-08-30 23:49:32 +0800

    Cleaned up codes

--- frontend/src/components/Payments/SplitInvite.tsx
+++ frontend/src/components/Payments/SplitInvite.tsx
@@ -12,10 +12,10 @@ const SplitInvite = ({ toPay }: any) => {
       <div className="bg-white shadow-[0_4px_12px_-5px_rgba(0,0,0,0.4)] w-full rounded-lg font-[sans-serif] overflow-hidden mx-auto mt-4">
         <div className="p-6">
           <p className="mt-2 mb-2 text-lg text-gray-500 leading-relaxed">
-            {details.initiator} and {toPay["splitters"].length - 1} other
-            splitter/s would like to split the cost for {toPay["name"]},
+            {details.initiator} and {details.splitters.length - 1} other
+            splitter/s would like to split the cost for {details.name},
             totaling $
-            {parseFloat(toPay["amount"]).toLocaleString(undefined, {
+            {parseFloat(details.totalAmount).toLocaleString(undefined, {
               maximumFractionDigits: 2,
             })}
           </p>
@@ -23,7 +23,7 @@ const SplitInvite = ({ toPay }: any) => {
             <Link
               type="button"
               className="px-5 py-2.5 rounded-full text-white text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-blue-800 active:bg-blue-700 self-center  -mb-4 mt-4"
-              to={"/payment/" + toPay["_id"]}
+              to={`/payment/${details.payment_id}`}
               state={details}
             >
               Split Pay Now
