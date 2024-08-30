501355a5232f6b72a4cf334391cc9d4357af0e32
Newton Tugadi <newtonboy@gmail.com>
2024-08-31 07:14:55 +0800

    Refactored date formatter

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,13 +1,12 @@
-export const formatDate = (datetime: string | number | Date) => {
-  if (datetime) {
-    const date = new Date(datetime);
+export const formatDate = (datePaid: string | number | Date) => {
+  if (datePaid) {
+    const date = new Date(datePaid);
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
     const year = String(date.getFullYear()).slice(-2);
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const formattedDate = `${month}-${day}-${year} ${hours}:${minutes}`;
-
     return formattedDate;
   }
 };
